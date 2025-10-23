0b8c83538164df936334cff9254dcd25ffcdc825
sujichowdary2003-cmd <sujichowdary2003@gmail.com>
2025-10-23 18:48:29 +0530

    Fix formatting issues in diabetes_app.py

--- diabetes_app.py.py
+++ diabetes_app.py.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #load the saved model
-loaded_model = pickle.load(open('D:/Diabetes  project/_trained.model.sav', 'rb'))
+loaded_model = pickle.load(open('D:/Diabetes project/_trained.model.sav', 'rb'))
 
 
 #creating a function for Detection
@@ -65,3 +65,4 @@ def main():
 if __name__=='__main_':
 
     main()
+
