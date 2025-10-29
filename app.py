a353e0e2728d2bc1bd5f70635434469e38ea8488
sujichowdary2003-cmd <sujichowdary2003@gmail.com>
2025-10-29 12:16:34 +0530

    Fix model loading path and improve code structure
    Corrected the model loading path and fixed indentation issues.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #load the saved model
-loaded_model = pickle.load(open(r'D:\Diabetes project\_trained_model.sav', 'rb'))
+loaded_model = pickle.load(open(r'D:\D:\Diabetes  project', 'rb'))
 
 
 
@@ -66,6 +66,7 @@ def main():
 if __name__=='__main_':
 
     main()
+
 
 
 
